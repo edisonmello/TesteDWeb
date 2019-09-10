9a5e75a06d996f94adbb833f5f685197e0c5430f
Edison MEllo <edisonmello@gmail.com>
2019-09-10 20:08:30 -0300

    Meu segundo arquivo

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+		include 'header.php';
+		$aula_atual = 'inicio';
+	?>
+
+
+	<body>
+
+
+		<h2>Índice de Aulas Web - Codificando na prática</h2>
+		<hr>
+		<small>Desenvolvimento Web II</small>
+	
+
+
+
+
+
+	<?php include 'functions/bottom_index.php'; ?>
+
+
+	</body>
+
+</html>
